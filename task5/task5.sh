e1fe1e5044f2aa0061136062ae3dac11cf7ad155
ZivHong <rock33332@gmail.com>
2021-10-28 23:28:47 +0800

    update code

--- task5/task5.sh
+++ task5/task5.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 export _pass=1234
-# head -c 1024 /dev/urandom > origin.txt
-# truncate -s 1024 origin.txt
 for j in cbc cfb ecb ofb; do  
     openssl enc -e -aes-128-$j -in origin.txt -out 1024-$j.txt -pbkdf2 -pass env:_pass
     python corrupt.py 1024-$j.txt 1024bad-$j.txt 
